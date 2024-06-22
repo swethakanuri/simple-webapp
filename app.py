5ee22c117ead585d1fe4b4866679e9edbe6e3b19
swethakanuri <61013408+swethakanuri@users.noreply.github.com>
2024-06-22 22:44:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome!"
+    return "Welcome-swetha-123!"
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',port=5000)
